c8bb27d2d64ede3f9425a769dc6258f006a567af
Erik <dex.foer@mail.ru>
2021-02-16 03:08:24 +0900

    ods

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,31 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже просмотрели?')
-console.log(numberOfFilms)
+const numberOfFilms = +prompt('Сколько фильмов вы просмотрели?')
 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: numberOfFilms == null
+    privat: false
 }
 
-const a = prompt('Один из последних просмотренных фильмов?', '')
-      b = prompt('На сколько оцените его?', '')
-personalMovieDB.movies[a] = b
+for(let i = 1; i < 2; i++) {
+    const a = prompt('Последний ваш просмотренный фильм?')
+          b = prompt('На сколько баллов вы оцените этот фильм?')
 
-console.log(personalMovieDB)
+    if(a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b
+        console.log('Done')
+    } else {
+        console.log('error')
+    }
+
+    if(personalMovieDB.count < 10) {
+        console.log('Мало фильмов ты посмотрел конечно')
+    } else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Нормально так')
+    } else if(personalMovieDB.count > 30) {
+        console.log('Много же фильмов ты посмотрел епта')
+    } else {
+        console.log('error')
+    }
+}
